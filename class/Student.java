62518aee782d6ca5ff42b8609f21748178ff3301
Idba1 <moniraislamself@gmail.com>
2025-01-19 05:53:39 +0600

    Java Instance Methods

--- class/Student.java
+++ class/Student.java
@@ -1,4 +1,19 @@
 public class Student {
     public String name; // instance variable
     public int id = 000;
+    public String clubName;
+
+    public void duingClass() { // instance method
+        System.out.println(name + " attend her OOP class");
+    }
+
+    public void clubActivity(String clubName) { // instance method
+        System.out.println(name + " join in " + clubName + " club");
+    }
+
+    public void showDetail() {
+        System.out.println("Student name is " + name + " and her id is " + id + " ");
+        duingClass();
+        clubActivity(clubName);
+    }
 }
--- class/Tester.java
+++ class/Tester.java
@@ -4,12 +4,19 @@ public class Tester {
         Student s1 = new Student();
         s1.name = "Bob";
         s1.id = 90;
+        s1.clubName = "Cyber Security";
         Student s2 = new Student();
         s2.name = "Carel";
-        // s2.id = 80;
-        System.out.println(s1.name);
-        System.out.println(s1.id);
-        System.out.println(s2.name);
-        System.out.println(s2.id);
+        s2.id = 80;
+        s2.clubName = "Data Science";
+        // System.out.println(s1.name);
+        // System.out.println(s1.id);
+        // System.out.println(s2.name);
+        // System.out.println(s2.id);
+        // s1.duingClass();
+        // s2.clubActivity("Cyber Security");
+        s1.showDetail();
+        System.out.println("--------------------------");
+        s2.showDetail();
     }
 }
