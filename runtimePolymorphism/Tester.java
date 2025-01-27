8f54dbbc9f45072735f11f8912d7b4cdfcbee5fb
Idba1 <moniraislamself@gmail.com>
2025-01-27 14:39:19 +0600

    Polymorphism | Upcasting | Downcasting | instanceOf Keyword

--- runtimePolymorphism/Tester.java
+++ runtimePolymorphism/Tester.java
@@ -1,18 +1,27 @@
 public class Tester {
     public static void displayInfo(Animal a) {
-        a.makeSound();
+        if (a instanceof Dog) {
+            Dog d = (Dog) a; // downCasting
+            d.makeSound();
+            d.bark();
+        } else {
+            a.makeSound();
+        }
     }
 
     public static void main(String[] args) {
         // reference_type variable = new obj()
-        // Animal a1 = new Dog(); //upcasting
+        // Animal a1 = new Dog(); //upCasting
         // Dog d1 = new Dog();
-        // Animal a1 = (Animal)d1; //upcasting
+        // Animal a1 = (Animal)d1; //upCasting
         // a1.makeSound();
 
         Animal a1 = new Animal();
         Dog d1 = new Dog();
         Cat c1 = new Cat();
+        // System.out.println(a1 instanceof Dog);
+        // System.out.println(d1 instanceof Animal);
+        displayInfo(a1);
         displayInfo(d1);
         displayInfo(c1);
     }
